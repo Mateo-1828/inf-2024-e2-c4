31fd23bbcf97bc680a057c095360b714805f993c
natys <bimbote12@gmail.com>
2024-07-08 18:45:47 -0300

    problema2

---
+++ problema2.py
@@ -0,0 +1,12 @@
+hermano1 = 26
+hermano2 = 21
+
+#obtencion de edades
+
+if hermano1 > hermano2:
+    print("El hermano1 es mayor que el hermano2")
+elif hermano2 > hermano1:
+    print("el hermano2 es mayor que al hermano1")
+else:
+    print("los hermano tienen la misma edad")    
+    
